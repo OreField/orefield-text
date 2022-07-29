2984120e160635b04f1f088f2918c11838717bff
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-07-30 00:55:27 +1000

    build: fix weird auto imports

--- build-logic/src/main/kotlin/orefield-text.java-conventions.gradle.kts
+++ build-logic/src/main/kotlin/orefield-text.java-conventions.gradle.kts
@@ -2,10 +2,6 @@
  * Copyright 2022 OreField - All Rights Reserved
  */
 
-import gradle.kotlin.dsl.accessors._3018ba51e2fe77536ad2914c7cacf0d3.implementation
-import gradle.kotlin.dsl.accessors._3018ba51e2fe77536ad2914c7cacf0d3.java
-import gradle.kotlin.dsl.accessors._3018ba51e2fe77536ad2914c7cacf0d3.testImplementation
-
 plugins {
     java
     `java-library`
