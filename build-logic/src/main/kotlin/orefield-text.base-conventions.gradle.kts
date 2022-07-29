98c7db8dea08c0842d8ace470c78dbc19d5fc18c
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-07-30 00:52:46 +1000

    build: remove jacoco

--- build-logic/src/main/kotlin/orefield-text.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/orefield-text.base-conventions.gradle.kts
@@ -2,11 +2,9 @@
  * Copyright 2022 OreField - All Rights Reserved
  */
 
-import org.gradle.kotlin.dsl.jacoco
 import org.gradle.kotlin.dsl.`maven-publish`
 
 plugins {
-    jacoco
     `maven-publish`
     signing
 }
